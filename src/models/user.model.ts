31c12be312db9b1da6d183ecf1582e48db65e984
yuriaresi <yuri.aresi10@gmail.com>
2024-01-11 20:15:11 -0300

    pagina de avaliaçoes do usuario logado

---
+++ src/models/user.model.ts
@@ -0,0 +1,5 @@
+export interface User {
+    id: string;
+    nome: string;
+    token: string;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,14 @@ import { Header } from "../components/Header";
 import { ListaAvaliacoes } from "../components/ListaAvaliacoes";
 import { Avaliacao } from "../models/avaliacao.model";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { User } from "../models/user.model";
 export const Home = () => {
   const [avaliacoes, setAvaliacoes] = useState<Avaliacao[]>([]);
+  const [user, setUser] = useState<User>();
+
+  const usuarioLogado = localStorage.getItem("user");
+  const navigate = useNavigate();
 
   useEffect(() => {
     //... chamada da API
@@ -22,14 +28,26 @@ export const Home = () => {
     //     console.log(error);
     //     alert("Error na requisição");
     //   });
+    if (!usuarioLogado) {
+      alert("Sessão expirada, faça o login novamente");
+      navigate("/login");
+      return;
+    }
 
-    listarAvaliacoes();
+    setUser(JSON.parse(usuarioLogado));
   }, []);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    listarAvaliacoes();
+  }, [user]);
 
   const listarAvaliacoes = async () => {
     try {
       const result = await axios.get(
-        "http://localhost:3333/aluno/4de59b49-e9e2-4c6e-ae5d-88b8d5835930/avaliacao"
+        `http://localhost:3333/aluno/${user?.id}/avaliacao`
       );
       console.log(result.data.data.avaliacoes);
       setAvaliacoes(result.data.data.avaliacoes);
@@ -40,6 +58,11 @@ export const Home = () => {
     }
   };
 
+  const realizarLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
     <>
       <Header />
@@ -48,8 +71,12 @@ export const Home = () => {
       <h2>Lista de Avaliações</h2>
 
       <ListaAvaliacoes avaliacoes={avaliacoes} />
-     
+
       <button onClick={listarAvaliacoes}>Atualizar Lista</button>
+
+      <br />
+
+      <button onClick={realizarLogout}>Logout</button>
     </>
   );
 };
